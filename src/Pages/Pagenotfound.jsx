f74d35861c34bf3bc4ea210be0a9120de4689b9a
Jisna1995 <jisnastephen29@gmail.com>
2025-11-05 13:11:16 +0530

    spelling-mistake

--- src/Pages/Pagenotfound.jsx
+++ src/Pages/Pagenotfound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Pagenotfound.css';
+import './pagenotfound.css';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
